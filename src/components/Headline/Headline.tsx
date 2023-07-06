989c805459e9e73c6b3e6a461e81feb2c8a248dd
Alisa Promthep <alisa.promthep@gmail.com>
2023-07-06 13:45:26 -0700

    headline mobile

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -8,10 +8,14 @@ const Headline = () => {
             flexDirection:'column',
             justifyContent:'center',
             }}>
-            <Typography variant="h1" align="center">
+            <Typography variant="h1" align="left"
+            sx={{margin:'1rem'}}
+            >
                 Find your new <span className="text-highlight">best friend</span>
             </Typography>
-            <Typography variant="body1" align="center">
+            <Typography variant="body1" align="left"
+            sx={{margin:'1rem'}}
+            >
                 Let's get started. Search pets from shelters, rescues, and individuals
             </Typography>
         </Container>
